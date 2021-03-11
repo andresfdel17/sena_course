6748c12b11e237505818f71a6474804883eda44a
Andres Felipe <andresfdel13@hotmail.com>
2021-03-11 15:06:35 -0500

    Actividad inicial curso

---
+++ Inicial/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Andrés Felipe Delgado <br/>";
+echo 1094952560 . "<br/>";
+echo "Buena tarde profe, como ya había dicho mi sistema no es windows por lo tanto mi entrono de desarrollo es distinto";
