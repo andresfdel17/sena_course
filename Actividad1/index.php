716e107e913045f2745c111c965afc32fdf6fcef
Andres Felipe <andresfdel13@hotmail.com>
2021-03-13 13:25:44 -0500

    Actividad1

---
+++ Actividad1/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Andrés Felipe Delgado <br/>", 1094952560 ,"<br/>Buena tarde profe, como ya había dicho mi sistema no es windows por lo tanto mi entrono de desarrollo es distinto";
--- Inicial/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "Andrés Felipe Delgado <br/>";
-echo 1094952560 . "<br/>";
-echo "Buena tarde profe, como ya había dicho mi sistema no es windows por lo tanto mi entrono de desarrollo es distinto";
